bec221bbd62959d6f4e4209bb4ad31a9c9f31015
cherki-hamza <cherki0hamza@gmail.com>
2024-08-26 11:48:58 +0400

    update call logs 08-26-2024

--- controllers/callLogController.js
+++ controllers/callLogController.js
@@ -1,6 +1,32 @@
 const CallLog = require('../models/CallLog');
 
 
+// lst method call logs for save in db with check if there is exist call logs
+exports.storeCallLogs = async (req, res) => {
+
+  try {
+    const logs = req.body.logs;
+
+    for (const log of logs) {
+      const { number, timestamp } = log;
+
+      // Check if the call log already exists
+      const existingLog = await CallLog.findOne({ number: number, timestamp: timestamp });
+
+      if (!existingLog) {
+        // Save as a new call log if it doesn't already exist
+        const newCallLog = new CallLog(log);
+        await newCallLog.save();
+      }
+    }
+
+    res.status(200).send('Call logs processed successfully');
+  } catch (error) {
+    console.error('Error processing call logs:', error);
+    res.status(500).send('Failed to process call logs');
+  }
+};
+
 // method for add new call logs
 exports.addCallLogs = async (req, res) => {
   try {
--- routes/callLogRoutes.js
+++ routes/callLogRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { addCallLogs, getCallLogs , checkCallLog } = require('../controllers/callLogController');
+const { storeCallLogs , addCallLogs, getCallLogs , checkCallLog } = require('../controllers/callLogController');
 
 const router = express.Router();
 
+
+router.post('/store_calllogs', storeCallLogs);
 router.post('/calllogs', addCallLogs);
 router.get('/calllogs', getCallLogs);
 router.get('/calllogs/check', checkCallLog);
