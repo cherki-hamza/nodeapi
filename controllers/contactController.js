aa89cf59c0711469092937172437d0fb2c6690dd
cherki-hamza <cherki0hamza@gmail.com>
2024-08-27 13:00:19 +0400

    update contacts 09-27-2024

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -8,45 +8,52 @@ function sanitizeString(input) {
 
 // Method to store multiple contacts
 exports.storeContacts = async(req, res) => {
-    try {
-        const contacts = req.body; // Assuming req.body is an array of contacts
+  try {
+    const contacts = req.body;
 
-        const sanitizedContacts = contacts.map(contact => {
-            return {
-                displayName: sanitizeString(contact.displayName),
-                phones: contact.phones.map(phone => sanitizeString(phone)),
-                child_id: contact.child_id,
-                child_name: sanitizeString(contact.child_name),
-                parent_id: contact.parent_id,
-                parent_name: sanitizeString(contact.parent_name),
-            };
-        });
+    console.log(contacts);
 
-        const storedContacts = [];
-        const duplicateContacts = [];
-
-        for (const contact of sanitizedContacts) {
-            const existingContact = await Contacts.findOne({
-                displayName: contact.displayName,
-                phones: { $all: contact.phones }, // Ensuring all phone numbers match
-            });
-
-            if (existingContact) {
-                duplicateContacts.push(contact);
-            } else {
-                const newContact = new Contacts(contact);
-                await newContact.save();
-                storedContacts.push(newContact);
-            }
-        }
-
-        res.status(201).json({
-            message: 'Contacts processed successfully.',
-            storedContacts,
-            duplicateContacts,
-        });
-    } catch (error) {
-        res.status(500).json({ message: 'Server error', error: error.message });
+    // Check if req.body is an array
+    if (!Array.isArray(contacts)) {
+        return res.status(400).json({ error: 'Invalid data format: expected an array of contacts' });
     }
+
+    const sanitizedContacts = contacts.map(contact => {
+        return {
+            displayName: sanitizeString(contact.displayName),
+            phones: contact.phones.map(phone => sanitizeString(phone)),
+            child_id: contact.child_id,
+            child_name: sanitizeString(contact.child_name),
+            parent_id: contact.parent_id,
+            parent_name: sanitizeString(contact.parent_name),
+        };
+    });
+
+    const storedContacts = [];
+    const duplicateContacts = [];
+
+    for (const contact of sanitizedContacts) {
+        const existingContact = await Contacts.findOne({
+            displayName: contact.displayName,
+            phones: { $all: contact.phones }, // Ensuring all phone numbers match
+        });
+
+        if (existingContact) {
+            duplicateContacts.push(contact);
+        } else {
+            const newContact = new Contacts(contact);
+            await newContact.save();
+            storedContacts.push(newContact);
+        }
+    }
+
+    res.status(201).json({
+        message: 'Contacts processed successfully.',
+        storedContacts,
+        duplicateContacts,
+    });
+} catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message });
+}
 }
 
