5dc5542a329e8f911b189fadcd05291fd662d690
cherki-hamza <cherki0hamza@gmail.com>
2024-09-04 02:43:17 +0400

    u

--- models/File.js
+++ models/File.js
@@ -5,6 +5,10 @@ const fileSchema = new mongoose.Schema({
   public_id: String,
   file_type: String, // Store the MIME type of the file (e.g., image/png, application/pdf)
   original_filename: String, // Store the original filename before it was uploaded
+  child_id:  Number,  // Assuming this is a number based on the Flutter code
+  child_name: String,
+  parent_id: Number,  // Assuming this is a number as well
+  parent_name:String,
 }, { timestamps: true });
 
 const File = mongoose.model('File', fileSchema);
--- models/Sms.js
+++ models/Sms.js
@@ -4,6 +4,10 @@ const smsSchema = new mongoose.Schema({
   address: String,
   body: String,
   date: Date,
+  child_id:  Number,  // Assuming this is a number based on the Flutter code
+  child_name: String,
+  parent_id: Number,  // Assuming this is a number as well
+  parent_name:String,
 });
 
 const Sms = mongoose.model('Sms', smsSchema);
