774f2b8f528a4897538789910c61284fb18a0175
cherki-hamza <cherki0hamza@gmail.com>
2024-09-05 18:21:30 +0400

    update the app child_id from int to string

--- models/App.js
+++ models/App.js
@@ -4,9 +4,9 @@ const appSchema = new mongoose.Schema({
   appName: { type: String },
   packageName: { type: String },
   icon: { type: String },
-  child_id: { type: Number },
+  child_id: { type: String },
   child_name: { type: String },
-  parent_id: { type: Number },
+  parent_id: { type: String },
   parent_name: { type: String },
 });
 
