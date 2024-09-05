7bc779227bcd58283a73ed04a70462022f0dc92b
cherki-hamza <cherki0hamza@gmail.com>
2024-09-05 13:18:31 +0400

    update the location

--- routes/locationRoutes.js
+++ routes/locationRoutes.js
@@ -3,6 +3,6 @@ const { saveLocation } = require('../controllers/locationController');
 
 const router = express.Router();
 
-router.post('/locations', saveLocation);
+router.post('/store_location', saveLocation);
 
 module.exports = router;
